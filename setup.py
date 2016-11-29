5de00a10c336046b0b1f72906e73667c716d2b4a
ezra <gregg.alb@gmail.com>
2016-11-29 12:31:41 +0200

    Add reqs

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
-import sys
 import os
+import sys
 from setuptools import setup, find_packages
 
 if sys.version_info < (3,0):
   sys.exit("Bandcamp_tagplayer requires python 3.")
 
 version = '0.0.1'
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+  required = f.read().splitlines()
 
 setup(
     name='bandcamp_tagplayer',
@@ -21,14 +24,7 @@ setup(
     url='http://github.com/greggparrish/bandcamp-tagplayer',
     packages=find_packages(),
     package_data={},
-    install_requires=[
-        'beautifulsoup4>=4.5.1',
-        'docopt>=0.6.2',
-        'lxml>=3.6.4',
-        'mutagen>=1.35.1',
-        'python-mpd2'>='0.5.5',
-        'slugify>=0.0.1',
-    ],
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'bandcamp-tagplayer=bandcamp_tagplayer.bandcamp_tagplayer:main',
